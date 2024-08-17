7aecb95216d246fcd380d00fe88219f90fa54007
azla mahryu <sgommccisbad72145@gmail.com>
2024-08-17 16:28:51 +0900

    #10 view test import class

---
+++ api/accounts/tests/test_user_views.py
@@ -0,0 +1,3 @@
+from rest_framework.test import APIRequestFactory,APITestCase, URLPatternsTestCase,force_authenticate,APIClient
+from django.urls import include, path, reverse
+from django.test import TestCase
