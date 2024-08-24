6ff619aaef7a4ca14a5f4ac2ad7affc971d8e326
azla mahryu <sgommccisbad72145@gmail.com>
2024-08-24 16:18:20 +0900

    #20 test code authentication login

--- api/accounts/tests/test_user_views.py
+++ api/accounts/tests/test_user_views.py
@@ -11,9 +11,11 @@ class JWTViewTest(APITestCase):
     def setUp(self):
         user1=User(
             email="tsukiji.not.service@netmeme.com",
+            is_active=True,
         )
         user2=User(
             email="ryutaro.nonomura@itmeme.co.jp",
+            is_active=True,
         )
         user1.set_password('xn39zksh32a')
         user1.save()
@@ -38,49 +40,46 @@ class JWTViewTest(APITestCase):
         self.user_email="tsukiji.not.service@netmeme.com"
         self.factory=APIRequestFactory()
         self.client=APIClient()
-
-    # @classmethod
-    # def setUpTestData(cls) -> None:
-    #     user1=User(
-    #         email="tsukiji.not.service@netmeme.com",
-    #     )
-    #     user2=User(
-    #         email="ryutaro.nonomura@itmeme.co.jp",
-    #     )
-    #     user1.set_password('xn39zksh32a')
-    #     user1.save()
-        
-    #     user2.set_password("k39dxmwsks")
-    #     user2.save()
-    #     UserProfile.objects.create(
-    #         nickname="築地市場営業しているニキ＆いないニキ",
-    #         user_profile=user1,
-    #         account_id="tsukiji_market_boy",
-    #         bio="東京の築地市場は営業していません！（営業しております）"
-    #     )
-        
-        
-    #     UserProfile.objects.create(
-    #         nickname="nonomura ryutaro",
-    #         user_profile=user2,
-    #         account_id="aaaaaaaaa",
-    #         bio="元兵庫県議会所属．俺は゛ね゛ぇ゛デュハハ．おんなじやおんなじや"
-    #     )
-        
-    #     return super().setUpTestData()
     
     def test_jwt_login(self):
         
-        data={
+        pdata={
             'email':self.user_email,
             'password':self.user_pw
         }
-        response=self.client.post(
-            '/auth/account/jwt/create/',
-            data,
-            format="json"
-            )
         
-        self.assertTrue("access" in response.json())
-        self.assertTrue("refresh" in response.json())
+        login_url=reverse('token_obtain_pair')
+        response=self.client.post(
+            login_url,
+            pdata,
+            format="json"
+        )
+        
+        # response=self.factory
+        
+        # self.assertTrue("access" in response)
+        # self.assertTrue("refresh" in response)
+        print(response)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+    
+    def test_jwt_refresh_token(self):
+        
+        pdata={
+            'email':self.user_email,
+            'password':self.user_pw
+        }
+        
+        login_url=reverse('token_obtain_pair')
+        login_response=self.client.post(
+            login_url,
+            pdata,
+            format="json"
+        )
+        
+        self.assertEqual(login_response.status_code,status.HTTP_200_OK)
+        
+        refresh_url=reverse('token_refresh')
+        refresh_response=self.client.post(
+            refresh_url,
+            
+        )
